cf6cfa0f89f225aa218d30c1157b41f75f755a65
Joaquim Perez <jivkim@gmail.com>
2023-04-17 21:38:48 +0200

    change route to matches

--- app.py
+++ app.py
@@ -9,9 +9,9 @@ cache = Cache(app, config={
     "CACHE_DEFAULT_TIMEOUT": 60 * 60 # 1 hour
 })
 
-@app.route("/")
+@app.route("/matches")
 @cache.cached()
-def hello_world():
+def matches():
     subprocess.check_output(["scrapy", "runspider", "spider.py", "-O", "matches.json"])
     with open("matches.json") as f:
         return f.read()
