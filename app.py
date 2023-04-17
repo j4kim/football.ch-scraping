7cdb78dc4b801e714fa90ba76e4fa57526fcdd84
Joaquim Perez <jivkim@gmail.com>
2023-04-17 21:37:29 +0200

    cache data

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 from flask import Flask
 import subprocess
+from flask_caching import Cache
+
 
 app = Flask(__name__)
+cache = Cache(app, config={
+    "CACHE_TYPE": "SimpleCache",
+    "CACHE_DEFAULT_TIMEOUT": 60 * 60 # 1 hour
+})
 
 @app.route("/")
+@cache.cached()
 def hello_world():
     subprocess.check_output(["scrapy", "runspider", "spider.py", "-O", "matches.json"])
     with open("matches.json") as f:
