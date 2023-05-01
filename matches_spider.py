6629ce9b567b61ff6ace55bf958cb3a77449846b
Joaquim Perez <jivkim@gmail.com>
2023-05-01 16:06:41 +0200

    variable renaming

--- matches_spider.py
+++ matches_spider.py
@@ -16,16 +16,16 @@ class Spider(scrapy.Spider):
         yield scrapy.Request(url, self.parse)
 
     def parse(self, response):
-        for matches in response.css('.row.spiel'):
-            fdate = matches.css('.date span::text').get()
+        for match in response.css('.row.spiel'):
+            fdate = match.css('.date span::text').get()
             yield {
                 'fdate': fdate,
                 # transform fdate from "Sa DD.MM.YYYY" to "YYYY-MM-DD"
                 'date': f'{fdate[9:13]}-{fdate[6:8]}-{fdate[3:5]}',
-                'time': clean(''.join(matches.css('.date::text').getall())),
-                'teamA': clean(''.join(matches.css('.teamA *::text').getall())),
-                'teamB': clean(''.join(matches.css('.teamB *::text').getall())),
-                'goalsA': clean(matches.css('.torA::text').get()),
-                'goalsB': clean(matches.css('.torB::text').get()),
-                'sppStatusText': matches.css('.sppStatusText::text').get(),
+                'time': clean(''.join(match.css('.date::text').getall())),
+                'teamA': clean(''.join(match.css('.teamA *::text').getall())),
+                'teamB': clean(''.join(match.css('.teamB *::text').getall())),
+                'goalsA': clean(match.css('.torA::text').get()),
+                'goalsB': clean(match.css('.torB::text').get()),
+                'sppStatusText': match.css('.sppStatusText::text').get(),
             }
