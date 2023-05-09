fe63bae69218fe742eb9f35423bc5aabcbe0f554
Joaquim Perez <jivkim@gmail.com>
2023-05-09 20:38:54 +0200

    app.py: 2 routes, 1 for fresh matches, the other for cached, remove flask caching module

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
 from flask import Flask
 import subprocess
-from flask_caching import Cache
 from flask_cors import CORS
 import config
 
 app = Flask(__name__)
-cache = Cache(app, config={
-    "CACHE_TYPE": "SimpleCache",
-    "CACHE_DEFAULT_TIMEOUT": 60 * 60 # 1 hour
-})
 CORS(app)
 
 @app.route("/")
 def index():
     return "Hello"
 
-@app.route("/matches")
-@cache.cached()
-def matches():
+@app.route("/matches/fresh")
+def freshMatches(self):
     command = [
         config.scrapy,
         "runspider", config.matches_spider,
@@ -26,5 +20,9 @@ def matches():
         "-a", f"team={config.team}",
     ]
     subprocess.run(command, check=True)
+    return self.cachedMatches()
+
+@app.route("/matches/cached")
+def cachedMatches():
     with open(config.matches_file) as f:
         return f.read()
