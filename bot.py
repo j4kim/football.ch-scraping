9a122621e27295c85f3b112ab78c93d47ab01bf7
Joaquim Perez <jivkim@gmail.com>
2023-12-05 14:15:55 +0100

    Bot class

--- bot.py
+++ bot.py
@@ -3,26 +3,38 @@ import config
 from os.path import getmtime
 import json
 
-def run():
-    command = [
-        config.scrapy,
-        "runspider", config.matches_spider,
-        "-O", config.matches_file,
-        "-a", f"club={config.club}",
-        "-a", f"team={config.team}",
-    ]
-    subprocess.run(command, check=True)
 
-def get():
-    with open(config.matches_file) as f:
-        return {
-            "matches": json.load(f),
-            "time": getmtime(config.matches_file),
-        }
+class Bot:
+    def __init__(self, club, team):
+        self.club = club
+        self.team = team
 
-def run_and_get():
-    run()
-    return get()
+    def run(self):
+        command = [
+            config.scrapy,
+            "runspider",
+            config.matches_spider,
+            "-O",
+            config.matches_file,
+            "-a",
+            f"club={self.club}",
+            "-a",
+            f"team={self.team}",
+        ]
+        subprocess.run(command, check=True)
 
-if __name__ == '__main__':
-    run()
+    def get():
+        with open(config.matches_file) as f:
+            return {
+                "matches": json.load(f),
+                "time": getmtime(config.matches_file),
+            }
+
+    def run_and_get(self):
+        self.run()
+        return self.get()
+
+
+if __name__ == "__main__":
+    bot = Bot(config.club, config.team)
+    bot.run()
